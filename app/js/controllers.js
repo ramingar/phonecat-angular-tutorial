65c665097bfbf852e5f22a5ce868c85b2fdc45fc
Rafael Minguet García <rminguet@gmail.com>
2015-06-21 17:59:52 +0200

    Get list of phones from a json using XmlHttpRequest ()

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -4,24 +4,10 @@
 
 angular.module('phonecatApp', [])
 
-.controller('PhoneListController', function ($scope) {
-    $scope.phones = [
-        {
-            'name': 'Nexus S',
-            'snippet': 'Fast just got faster with Nexus S.',
-            'age': '1'
-        },
-        {
-            'name': 'Motorola XOOM™ with Wi-Fi',
-            'snippet': 'The Next, Next Generation tablet.',
-            'age': '2'
-        },
-        {
-            'name': 'MOTOROLA XOOM™',
-            'snippet': 'The Next, Next Generation tablet.',
-            'age': '3'
-        }
-    ];
+.controller('PhoneListController', ['$scope', '$http', function ($scope, $http) {
+    $http.get('phones/phones.json').success(function (data) {
+        $scope.phones = data;
+    });
 
     $scope.orderPhone = 'age';
-});
+}]);
